dbe5d512de455bb551a7895fe8de7eeba73c848e
NikhilN-17 <nikhil.nn.24.beai@acharya.ac.in>
2025-05-23 19:21:14 +0200

    Create Charactergrid.py

---
+++ Charactergrid.py
@@ -0,0 +1,16 @@
+#charactergrid
+print("Nikhil N,USN:1AY24AI075,SEC:O")
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for y in range(len(grid[0])):
+    for x in range(len(grid)):
+        print(grid[x][y], end='')
+    print()
